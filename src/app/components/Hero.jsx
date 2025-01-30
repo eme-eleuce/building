4d6241bcd69600b1a2f6f552b3f9893cbe1073ac
Luis Mora <luis.iarmolen@gmail.com>
2025-01-30 13:06:42 -0500

    or

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {IoIosArrowDown} from 'react-icons/io';
 import fondo from '@/app/photos/fondoi.jpg';
-
+import { MdOutlineConstruction } from "react-icons/md";
 
 
 const Hero = () => {
@@ -31,10 +31,15 @@ const Hero = () => {
           alt="/"
           width={400}
           height={400}
-          className="w-[300px] h-[300px] md:w-78 md:h-78 lg:w-[450px] lg:h-[450px]"
+          className="w-[300px] h-[300px] md:w-78 md:h-78 lg:w-[450px] lg:h-[450px] mb-5 lg:mb-12"
         />
       </div>
-      <div className='absolute justify-center mt-[36rem] lg:mt-[34.5rem] md:mt-[34rem] text-white cursor-pointer'>
+      <div className='absolute justify-center mt-[26rem] lg:mt-[29rem] md:mt-[34rem] px-3'>
+      <p className='text-white text-xl font-bold flex items-center gap-2'>
+            Sitio en construcción <MdOutlineConstruction size={47} />
+      </p>
+      </div>
+      <div className='absolute justify-center mt-[36rem] lg:mt-[37rem] md:mt-[34rem] text-white cursor-pointer'>
             <Link href='#section1' scroll={true}>
                <IoIosArrowDown size={60}/>
             </Link>
