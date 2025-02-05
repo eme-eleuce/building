86a0a8d61964e2a329c756caf11ad0b81dac8c43
Luis Mora <luis.iarmolen@gmail.com>
2025-02-05 14:50:33 -0500

    wednesday

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,71 +1,80 @@
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { BsLinkedin, BsInstagram } from 'react-icons/bs'
-import Link from 'next/link'
 import Image from 'next/image'
 
-
 const Footer = () => {
-
     return (
-   
-        <footer>
-  <div className="max-w-[1240px] px-2 py-8 mx-auto space-y-8 sm:px-4 lg:space-y-10 lg:px-3">
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-      {/* Parte izquierda (logo, información y redes sociales) */}
-      <div className="lg:ml-8">
-        {/* Logo */}
-        <div>
-          <Image
+        <footer className="bg-white px-8 md:px-4">
+            <div className="max-w-[1240px] mx-auto py-8">
+                {/* Contenedor principal */}
+                <div className="flex flex-col space-y-8">
+                    {/* Logo */}
+                    <div>
+                        <Image
                             src="/pachar-h.png"
                             alt="/"
                             width={300}
                             height={300}
                             className="w-[230px] h-[130px] lg:w-[270px] lg:h-[150px]"
-                          />
-        </div>
+                        />
+                    </div>
 
-        {/* Información */}
-        <div className="max-w-xs ml-8 lg:ml-0 text-gray-500 text-left md:text-left">
-          <p className='text-xl font-bold'>Av. Francisco de Orellana #111 y Av. Plaza Dañín, Oficina 52</p>
-          <p className='text-lg'>Guayaquil, Ecuador</p>
-          <p className="flex items-center justify-start">
-            <FaPhoneSquareAlt size={20} className="mr-2"/> +593 93 9928517
-          </p>
-        </div>
+                    {/* Direcciones */}
+                    <div className="flex flex-col md:flex-row md:space-x-16">
+                        {/* Dirección Guayaquil */}
+                        <div className="mb-12 md:mb-0">
+                            <p className="text-xl font-bold text-gray-800">
+                                Av. Francisco de Orellana #111 y Av. Plaza Dañín, Oficina 52
+                            </p>
+                            <p className="text-lg text-gray-800 mt-2">
+                                Guayaquil, Ecuador
+                            </p>
+                        </div>
 
-        {/* Redes sociales */}
-        <ul className="flex gap-6 mt-8 ml-8 lg:ml-0 items-center">
-          <li>
-            <a
-              href="https://www.linkedin.com/company/pacchar/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:opacity-75"
-            >
-              <BsLinkedin size={25} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/pacchar.ec/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:opacity-75"
-            >
-              <BsInstagram size={25} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+                        {/* Dirección Quito */}
+                        <div>
+                            <p className="text-xl font-bold text-gray-800">
+                                Av. De Los Shyris E9-38 y Bélgica
+                            </p>
+                            <p className="text-xl font-bold text-gray-800">
+                                Edificio Shyris Century, Piso 12, Oficina 12 B
+                            </p>
+                            <p className="text-lg text-gray-800 mt-2">
+                                Quito, Ecuador
+                            </p>
+                        </div>
+                    </div>
 
-    {/* Derechos de autor */}
-    <p className="text-sm font-bold text-[#006535] lg:ml-5">
-      &copy; Pacchar. All rights reserved.
-    </p>
-  </div>
-</footer>
+                    {/* Teléfono */}
+                    
 
+                    {/* Redes sociales */}
+                    <div className="flex space-x-6">
+                        <a
+                            href="https://www.linkedin.com/company/pacchar/"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="text-gray-700 transition hover:opacity-75"
+                        >
+                            <BsLinkedin size={25} />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/pacchar.ec/"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="text-gray-700 transition hover:opacity-75"
+                        >
+                            <BsInstagram size={25} />
+                        </a>
+                    </div>
+
+                    {/* Copyright */}
+                    <p className="text-sm font-bold text-[#006535]">
+                        &copy; Pacchar. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
--- src/app/components/Section1.jsx
+++ src/app/components/Section1.jsx
@@ -1,77 +1,87 @@
-
 import Image from "next/image";
 import { IoDocumentTextOutline } from "react-icons/io5";
+import { MdMailOutline } from "react-icons/md";
 
 const Section1 = () => {
   return (
     <section id="section1" className="w-full min-h-screen py-8">
-    <h2 className="text-center text-5xl font-bold  mt-1 mb-8">Links</h2>
-    <div className="flex flex-col items-center pb-10 space-y-8 px-5">
-      {/* Primer Documento */}
-      <div className="flex flex-col items-center p-3  border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-md md:max-w-xl">
-        {/* Ícono de PDF */}
-        <Image
-                  src="/pachar-h.png"
-                  alt="/"
-                  width={300}
-                  height={300}
-                  className="w-[230px] h-[130px] lg:w-[270px] lg:h-[150px]"
-                />
-        <p className="text-center mb-4 font-bold">
-              Sobre Nosotros / Nuestra Empresa
-            </p>
-        {/* Enlace a Google Drive */}
-        <a
-          href="https://drive.google.com/file/d/1_57rhCVYCaUvmmxsX2R5Wttzprik7IeP/view?usp=drivesdk" 
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-[#007f00] text-white px-4 py-2 rounded-lg hover:bg-[#00b200] transition-colors "
-        >
-          Haz click aquí
-        </a>
-      </div>
+      <h2 className="text-center text-5xl font-bold mt-1 mb-12 py-6">Links</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-5 pb-10 place-items-center">
+        {/* Primer Documento */}
+        <div className="flex flex-col items-center justify-between p-6 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-2xl h-[300px]">
+          <Image
+            src="/pachar-h.png"
+            alt="/"
+            width={300}
+            height={300}
+            className="w-[250px] h-[150px] object-contain"
+          />
+          <p className="text-center mb-4 font-bold text-lg md:text-xl">
+                Sobre Nosotros / Nuestra Empresa
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1_57rhCVYCaUvmmxsX2R5Wttzprik7IeP/view?usp=drivesdk" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-[#007f00] text-white px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl rounded-lg hover:bg-[#00b200] transition-colors"
+          >
+            Haz click aquí
+          </a>
+        </div>
 
-      {/* Segundo Documento */}
-      <div className="flex flex-col items-center p-3 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-md md:max-w-xl">
-        {/* Ícono de PDF */}
-        <IoDocumentTextOutline size={80} className=" mb-2 text-gray-800" />
-        <p className="text-center mb-4 font-bold">
-            Ficha técnica de Pacchar Biochar
-            </p>
-        {/* Enlace a Google Drive */}
-        <a
-          href="https://drive.google.com/file/d/1H06HcNyHEfWYC0m_tqurrbIAyqwvWZNl/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-[#007f00] text-white px-4 py-2 rounded-lg hover:bg-[#00b200] transition-colors "
-        >
-          Ver Documento
-        </a>
+        {/* Segundo Documento */}
+        <div className="flex flex-col items-center justify-between p-6 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-2xl h-[300px]">
+          <IoDocumentTextOutline size={150} className="text-gray-800" />
+          <p className="text-center mb-4 font-bold text-lg md:text-xl">
+              Ficha técnica de Pacchar Biochar
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1H06HcNyHEfWYC0m_tqurrbIAyqwvWZNl/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-[#007f00] text-white px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl rounded-lg hover:bg-[#00b200] transition-colors"
+          >
+            Ver Documento
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center justify-between p-6 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-2xl h-[300px]">
+          <Image
+            src="/image.png"
+            alt="/"
+            width={300}
+            height={300}
+            className="w-[250px] h-[150px] object-contain"
+          />
+          <p className="text-center mb-4 font-bold text-lg md:text-xl">
+               Nuestros aliados de sostenibilidad 
+          </p>
+          <a
+            href="https://wayllaec.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-[#007f00] text-white px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl rounded-lg hover:bg-[#00b200] transition-colors"
+          >
+            RAIN FOREST
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center justify-between p-6 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-2xl h-[300px]">
+          <MdMailOutline size={150} className="text-gray-800" />
+          <p className="text-center mb-4 font-bold text-lg md:text-xl">
+               Información de Contacto
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1I7KeiapbMn_TDemlphuwQ3oExJqTriyz/view"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-[#007f00] text-white px-6 py-3 md:px-8 md:py-4 text-lg md:text-xl rounded-lg hover:bg-[#00b200] transition-colors"
+          >
+            CONTACTO
+          </a>
+        </div>
       </div>
-      <div className="flex flex-col items-center p-3 border-4 border-gray-600 rounded-lg shadow-md bg-white w-full max-w-md md:max-w-xl">
-        {/* Ícono de PDF */}
-        <Image
-                  src="/image.png"
-                  alt="/"
-                  width={300}
-                  height={300}
-                  className="w-[260px] h-[90px] lg:w-[300px] lg:h-[100px]"
-                />
-        <p className="text-center mb-4 font-bold">
-             Nuestros aliados de sostenibilidad 
-            </p>
-        {/* Enlace a Google Drive */}
-        <a
-          href="https://wayllaec.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-[#007f00] text-white px-4 py-2 rounded-lg hover:bg-[#00b200] transition-colors "
-        >
-          RAIN FOREST
-        </a>
-      </div>
-    </div>
-  </section>
+    </section>
   );
 }
 
