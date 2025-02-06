35d969be8f25a6c66766cc749032858ec1049a12
Luis Mora <luis.iarmolen@gmail.com>
2025-02-06 13:24:03 -0500

    seis_deldos

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,26 +1,54 @@
-
-
+"use client"
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import {IoIosArrowDown} from 'react-icons/io';
 import fondo from '@/app/photos/fondoi.jpg';
+import foto1 from '@/app/photos/1.jpg';
+import foto2 from '@/app/photos/2.jpg';
+import foto3 from '@/app/photos/3.jpg';
+import foto4 from '@/app/photos/4.jpg';
 import { MdOutlineConstruction } from "react-icons/md";
 import logo from '@/app/photos/landing.png';
 
 const Hero = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [fondo, foto4, foto2, foto3, foto1];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => 
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000); // Cambia imagen cada 5 segundos
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="relative h-screen flex items-center justify-center" >
-      {/* Imagen de fondo */}
+      {/* Slideshow de imágenes de fondo */}
       <div className="absolute inset-0">
-        <Image
-          src={fondo}
-          alt="/"
-          fill
-          objectFit="cover"
-          priority
-          quality={70}
-          placeholder='blur'
-        />
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            alt="/"
+            fill
+            objectFit="cover"
+            priority={index === 0}
+            quality={70}
+            placeholder='blur'
+            className={`
+              transition-all 
+              duration-1000 
+              ${currentImageIndex === index 
+                ? 'opacity-100 brightness-100'
+                : 'opacity-0 brightness-60'
+              }
+            `}
+          />
+        ))}
         <div className="absolute inset-0 bg-black opacity-30"></div>
       </div>
 
@@ -40,7 +68,7 @@ const Hero = () => {
       </p>
       </div>
       <div className='absolute justify-center mt-[26rem] lg:mt-[30rem] md:mt-[34rem] px-3'>
-        <p className='text-white text-xl md:text-2xl font-bold flex items-center text-center gap-2 '>¡Pronto habrá mucho más para ti!</p>
+        <p className='text-white text-xl md:text-2xl font-bold flex items-center text-center gap-2 '>¡Pronto habrá mucho más para ti!</p>
         </div>
       <div className='absolute justify-center mt-[36rem] lg:mt-[37rem] md:mt-[34rem] text-white cursor-pointer'>
             <Link href='#section1' scroll={true}>
